05108feca27e6099f5f6e75d915d548129823493
Tyler Long <tyler4long@gmail.com>
2018-12-26 11:16:44 +0800

    handleMessage4Bot

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ import express from 'express'
 const skill = {}
 
 skill.handle = async event => {
-
+  switch (event.type) {
+    case 'Message4Bot':
+      handleMessage4Bot(event)
+      break
+    default:
+      break
+  }
+}
+const handleMessage4Bot = async event => {
+  const { text, group, bot } = event
+  if (text === 'google authorize') {
+    await bot.sendMessage(group.id, { text: 'OK, let\'s do it' })
+  }
 }
 
 const app = express()
